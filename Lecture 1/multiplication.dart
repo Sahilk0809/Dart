6f05cbe7d7afe76c4d50d3d9d145aba2ed0573d1
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-03-06 18:26:15 +0530

    Update multiplication.dart

--- Lecture 1/multiplication.dart
+++ Lecture 1/multiplication.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 
 void main()
 {
+  print("Enter values to multiply : ");
   stdout.write("Enter a : ");
   int a = int.parse(stdin.readLineSync()!);
   stdout.write("Enter b : ");
   int b = int.parse(stdin.readLineSync()!);
 
-  print(a * b);
+  print("$a * $b = ${a * b}");
 }
