c94c0bdbe395ec2905b686ac645b64c4a6423385
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-03-06 18:29:02 +0530

    Update multiplication_table.dart

--- Lecture 1/multiplication_table.dart
+++ Lecture 1/multiplication_table.dart
@@ -5,8 +5,9 @@ void main()
   stdout.write("Enter n : ");
   int n = int.parse(stdin.readLineSync()!);
 
+  print("Multiplication table of $n");
   for(int i=1; i<=10; i++)
   {
-    print(n * i);
+    print("$n * $i = ${n * i}");
   }
 }
