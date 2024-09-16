f2ec4c6d5fcd9ec2fcc1fac4df6a6b73ec735b53
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-09-16 21:55:41 +0530

    Add files via upload

---
+++ Array/plus_one.dart
@@ -0,0 +1,16 @@
+List<int> plusOne(List<int> digits) {
+  for (int i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    
+    digits[i] = 0;
+  }
+  digits.insert(0, 1);
+  return digits;
+}
+
+void main(){
+  print(plusOne([1, 2, 3]));
+}
